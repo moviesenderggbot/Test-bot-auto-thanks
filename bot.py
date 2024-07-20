14edf422a33c817bfaf28caba1ec697409e074f9
Unique Shadows <149162089+unique-shadows@users.noreply.github.com>
2024-07-20 19:55:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from os import environ
 
 
 # Configuration - replace with your own values
-api_id = environ.get("api_id", "24491383")
-api_hash = environ.get("api_hash", "78e18eba669cc519ffd7a3c89f9ed32a")
-bot_token = environ.get("bot_token", "7374373768:AAH0rgmygV-Xee3Nz2Yf_WyytUlgHBGR5O4")
+api_id = environ.get("api_id", "")
+api_hash = environ.get("api_hash", "")
+bot_token = environ.get("bot_token", "")
 
 
 # Initialize the bot
